8a96d22338c61a43c05ad9495fea89377e6f07b1
727817393@qq.com <727817393@qq.com>
2017-12-09 17:22:14 +0800

    部署调试

--- common/config/main.php
+++ common/config/main.php
@@ -3,6 +3,13 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'language' => 'zh-CN',
     'components' => [
+    	'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=w.rdc.sae.sina.com.cn;dbname=app_notname',
+            'username' => 'mxl5z0lkjz',
+            'password' => 'kzwyzxjzjywz4jhhxz35kw5iwz3kkkm5wxwh3w2j',
+            'charset' => 'utf8',
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
